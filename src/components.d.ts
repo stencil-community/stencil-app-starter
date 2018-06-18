f87f1fe81a6c0e08176f81ee6bfc7c01f91414af
Josh Thomas <jthoms1@gmail.com>
2018-06-18 10:07:48 -0500

    chore(): update to the latest release of stencil.

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,9 +13,12 @@ declare global {
   }
   namespace JSXElements {}
 
+  interface HTMLElement {
+    componentOnReady?: () => Promise<this | null>;
+  }
+
   interface HTMLStencilElement extends HTMLElement {
     componentOnReady(): Promise<this>;
-    componentOnReady(done: (ele?: this) => void): void;
 
     forceUpdate(): void;
   }
@@ -24,6 +27,7 @@ declare global {
 }
 
 import '@stencil/router';
+import '@stencil/state-tunnel';
 
 import {
   MatchResults,
