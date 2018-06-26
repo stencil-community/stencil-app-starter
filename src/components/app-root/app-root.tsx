a72814e9303925d341b613eee3c46457ced7f111
Josh Thomas <jthoms1@gmail.com>
2018-06-26 13:25:16 -0500

    chore(): updated versions and added router-switch.

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -16,11 +16,10 @@ export class AppRoot {
 
         <main>
           <stencil-router>
-            <stencil-route url='/' component='app-home' exact={true}>
-            </stencil-route>
-
-            <stencil-route url='/profile/:name' component='app-profile'>
-            </stencil-route>
+            <stencil-route-switch scrollTopOffset={0}>
+              <stencil-route url='/' component='app-home' exact={true} />
+              <stencil-route url='/profile/:name' component='app-profile' />
+            </stencil-route-switch>
           </stencil-router>
         </main>
       </div>
