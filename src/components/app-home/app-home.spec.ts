e99546dbba592a6e1c037cce32b513394ed9b671
Ken Sodemann <ken@ionic.io>
2018-09-27 15:26:57 -0500

    fix(unit-tests): remove the rendering tests

--- src/components/app-home/app-home.spec.ts
+++ src/components/app-home/app-home.spec.ts
@@ -1,24 +1,7 @@
-import { TestWindow } from '@stencil/core/testing';
 import { AppHome } from './app-home';
 
 describe('app', () => {
-  it('should build', () => {
+  it('builds', () => {
     expect(new AppHome()).toBeTruthy();
-  });
-
-  describe('rendering', () => {
-    let element: HTMLAppHomeElement;
-    let testWindow: TestWindow;
-    beforeEach(async () => {
-      testWindow = new TestWindow();
-      element = await testWindow.load({
-        components: [AppHome],
-        html: '<app-home></app-home>'
-      });
-    });
-
-    it('creates the element', () => {
-      expect(element).toBeTruthy();
-    });
   });
 });
--- src/components/app-profile/app-profile.spec.ts
+++ src/components/app-profile/app-profile.spec.ts
@@ -1,41 +1,7 @@
-import { TestWindow } from '@stencil/core/testing';
 import { AppProfile } from './app-profile';
 
 describe('app-profile', () => {
-  it('should build', () => {
+  it('builds', () => {
     expect(new AppProfile()).toBeTruthy();
-  });
-
-  describe('rendering', () => {
-    let element: HTMLAppProfileElement;
-    let testWindow: TestWindow;
-    beforeEach(async () => {
-      testWindow = new TestWindow();
-      element = await testWindow.load({
-        components: [AppProfile],
-        html: '<app-profile></app-profile>'
-      });
-    });
-
-    it('should not render any content if there is not a match', async () => {
-      await testWindow.flush();
-      expect(element.textContent).toEqual('');
-    });
-
-    it('should work with a name passed', async () => {
-      element.match = {
-        path: '',
-        url: '',
-        isExact: false,
-        params: {
-          name: 'stencil'
-        }
-      };
-
-      await testWindow.flush();
-      expect(element.textContent).toEqual(
-        'Hello! My name is stencil. My name was passed in through a route param!'
-      );
-    });
   });
 });
